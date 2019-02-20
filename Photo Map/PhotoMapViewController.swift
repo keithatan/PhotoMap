ac1e2a0be5e0aea0b13ca7404c0b4396bd2bf1e8
keithatan <tan230@purdue.edu>
2019-02-20 18:30:59 -0500

    Implemented MapView

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -11,10 +11,19 @@ import MapKit
 
 class PhotoMapViewController: UIViewController, LocationsViewControllerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // One degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        // San Francisco Lat, Long = latitude: 37.783333, longitude: -122.416667
+        let mapCenter = CLLocationCoordinate2D(latitude: 37.783333, longitude: -122.416667)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: mapCenter, span: mapSpan)
+        // Set animated property to true to animate the transition to the region
+        mapView.setRegion(region, animated: false)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
